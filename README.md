ecb4f1f13527b845c92d553071ebf4f348f5b2ad
Leandro Machado <sr.machado@gmail.com>
2018-10-19 16:40:00 -0300

    The biggest change:
    SSL added;
    Validation if a user has poured before;
    Validation by address;
    Validation by IP;
    New thema (and support to change the themes with one line only);
    Admin dashboard;
    Right Panel (future lastest tweets);
    Express Server;
    Random prizes from 15 HTML to 1500 HTML;
    Hability to force run the prize;

--- README.md
+++ README.md
@@ -1,28 +1,26 @@
-# htmlfaucet
+cryptofaucet-node
+=================
 
-htmlfaucet is a service which provides testnet coins for testing/development purposes.
+What is it?
+----
+Fully customizable Bitcoin / Litecoin / altcoin faucet built on NodeJS, Express 3, and Bootstrap.
 
+Configuration
+----
+Set global configuration options in config.js 
 
-## Installation
+Installation
+----
 
-Install node.js on your computer.
-You can install it from [nodejs.org/download](http://nodejs.org/download)
+    npm install
+    
+This will install all the necessary dependencies
+    
+Run
+----
 
-Install mongodb on your computer.
-You can install it from [mongodb.org/downloads](https://www.mongodb.org/downloads)
+    node app.js
+    
+Or using forever:
 
-Make sure both are running.
-
-You can install dependencies with the command:
-'npm install'
-
-## Cloning
-You can clone the repository from GitHub as well. Once you find the repository
-run the command:
-'git clone https://github.com/machado-rev/htmlfaucet.git'
-
-
-## Run the file
-Open your terminal and find the directory you saved htmlfaucet under.
-Run the command:
-'node app.js'
+    forever start app.js
--- app.js
+++ app.js
@@ -1,122 +1,94 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var lessMiddleware = require('less-middleware');
-var bodyParser = require('body-parser');
-var http = require('http');
-var fs = require('fs');
-var config = require('./config/defaults.js');
-var bson = require('bson');
+/**************************************
 
-var app = express();
+	Bitcoin / Litecoin / Altcoin Faucet
+	April 2014, clapyohands <clapyohands1@gmail.com>
+	https://github.com/clapyohands/cryptofaucet-node
 
-var mongoose = require('mongoose');
-var db = mongoose.connection;
+	See config.js for Configuration Variables
+	Edit views/inc/nav.pug to add navigation elements
+	Edit views/inc/rightcol.pug and views/inc/leftcol.pug to manage advertisements and other text in the sidebars
 
- var configura = {
+	Requirements:
+	express 3.x (not tested with express 4)
+	pug
+	node-bitcoin
+	iz
+	nedb
+	querystring
+
+***************************************/
+var configura = {
     protocol: 'http',
-    user: 'leandro',
-    pass: '260023',
+    user: 'your_rpc_user_here',
+    pass: 'your_rpc_password_here',
     host: '127.0.0.1',
-    port: '4889',
+    port: 'your_rpc_port_here',
   };
 
 var RpcClient = require('bitcoin-core');
 var btcClient = new RpcClient(configura);
+//global
+config = require('./config')
+,iz = require('iz')
+,bitcoin = require('bitcoin-core')
+,client = new RpcClient(configura)
+,db = require('./db')(config.database)
+,processor = require('./processor');
 
-db.on('error', console.error);
-db.once('open', function() {
-  // create schemas and models here? maybe not.
-});
 
-mongoose.connect('mongodb://localhost:27017/faucet');
+//local
+var express = require('express')
+	,fs = require('fs')
+	,routes = require('./routes')
+ 	,http = require('http')
+ 	,https = require('https')
+	,path = require('path')
+	,app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// certicates
+var privateKey = fs.readFileSync( 'privatekey.pem' );
+var certificate = fs.readFileSync( 'certificate.pem' );
+var credentials = {key: privateKey, cert: certificate};
+
+//Express Configuration
+//app.set('port', process.env.PORT || 8080); // What port to use to the webserver -> define here
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-
-//app.configure(function() {
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-var bootstrapPath = path.join(__dirname, 'node_modules', 'bootstrap', 'less');
-var bootswatchPath = path.join(__dirname, 'public', 'stylesheets');
-app.use(lessMiddleware(path.join(__dirname, 'public'), {
-  dest: path.join(__dirname, 'public'),
-  parser: {
-    paths: [bootswatchPath, bootstrapPath],
-  }
-}));
-app.use(express['static'](path.join(__dirname, 'public')));
-//});
-//app.use(less(path.join(__dirname, 'public/stylesheets', 'less'), {
-//  paths  : [bootstrapPath],
-//  dest   : path.join(__dirname, 'public', 'stylesheets'),
-//  preprocess: {
-//    path: function(pathname, req) {
-//      return pathname.replace('/stylesheets/', '/');
-//    }
-//  },
-//  debug: true
-//}));
-
-//app.use(lessMiddleware(path.join(__dirname, 'public'), {
-//  parser: {
-//    paths: [TWITTER_BOOTSTRAP_PATH]
-//  }
-//}));
-//app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', routes);
-//app.use('/users', users);
-
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//    var err = new Error('Not Found');
-//    err.status = 404;
-//    next(err);
-//});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.use(express.logger('dev'));
+//app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({ secret: config.session_secret }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.csrf());
+app.use(function(req, res, next){
+	//res.cookie('TOKEN', req.csrfToken());
+	res.locals.csrftoken = req.csrfToken();
+	next();
 });
+app.disable('x-powered-by');
+app.use(express.errorHandler());
 
-//module.exports = app;
 
-fs.readdirSync('./controllers').forEach(function(file) {
-  if(file.substr(-3) == '.js') {
-    route = require('./controllers/' + file);
-    route.controller(app);
-  }
-});
+// Express Routes
+app.get('/', routes.index);
+app.get('/captcha', routes.captcha);
+app.get('/submit', function(req,res){res.redirect(302,'/')});
+app.post('/submit', routes.submit);
+app.get(config.dashboard.path, routes.dashboard);
+app.post(config.dashboard.path, routes.dashboard);
 
-http.createServer(app).listen(8090, function() {
-  console.log('htmlfaucet server listening on port ' + 8090);
-});
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+// For http
+httpServer.listen(9090);
+// For https
+httpsServer.listen(8090);
+
+
+
+// Run Faucet Processor
+processor.start();
+
